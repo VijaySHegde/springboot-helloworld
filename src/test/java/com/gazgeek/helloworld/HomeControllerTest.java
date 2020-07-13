9390249da84f0e7f8e70728580c023711a579cb9
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-07-13 22:38:30 +0530

    Update HomeControllerTest.java

--- src/test/java/com/gazgeek/helloworld/HomeControllerTest.java
+++ src/test/java/com/gazgeek/helloworld/HomeControllerTest.java
@@ -26,7 +26,7 @@ public class HomeControllerTest {
 
     @Test
     public void helloWorld() {
-        getRequest("/")
+        getRequest("/hello")
             .assertStatusCode(OK)
             .assertResponseBody("Hello from GazGeek!");
     }
