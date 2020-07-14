52007ca49a8541590951b8dc6a3265f0b94ac635
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2020-07-14 11:08:42 +0530

    added servlet initializer

--- src/main/java/com/gazgeek/helloworld/Application.java
+++ src/main/java/com/gazgeek/helloworld/Application.java
@@ -3,10 +3,11 @@ package com.gazgeek.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+@SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
-public class Application {
+public class Application extends ServletInitializer{
 
 
     public static void main(String[] args) throws Exception {
